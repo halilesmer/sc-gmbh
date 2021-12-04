fd4602b9a79ff4d2cc008c0fa12c14b157552437
halilesmer <taximinator@gmail.com>
2021-12-04 21:36:28 +0100

    changed Photos and added impressum link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const networks = this.props.data.social.map(function (network) {
+    {/* const networks = this.props.data.social.map(function (network) {
       return (
         <li key={network.name}>
           <a href={network.url}>
@@ -13,21 +13,21 @@ class Footer extends Component {
           </a>
         </li>
       );
-    });
+    }); */}
 
     return (
       <footer>
         <div className="row">
           <Fade bottom>
             <div className="twelve columns">
-              <ul className="social-links">{networks}</ul>
+              {/* <ul className="social-links">{networks}</ul> */}
 
               <ul className="copyright">
-                <li>&copy; Copyright 2021 Nordic Giant</li>
+
                 <li>
                   Design by{" "}
-                  <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
+                  <a title="impressum" href="http://www.styleshout.com/">
+                    Impressum
                   </a>
                 </li>
               </ul>
